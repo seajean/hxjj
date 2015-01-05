178adbb9b35bce238a225db7c92d6e38c93c8c39
Shakna Israel <s4b3r6@gmail.com>
2015-01-04 19:40:44 -0500

    Express on VPS

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/', function (req, res){
 	});
 
 // Set the Server Up
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log('App is listening at http://%s:%s', host, port)
