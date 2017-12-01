2c674c3584d51ba6ba1eae6146b345f911b45686
梦初 <xiaojie.yxj@alibaba-inc.com>
2017-12-01 11:46:57 +0800

    增加文件

--- app.js
+++ app.js
@@ -18,34 +18,33 @@ app.use('/', express.static('assets'));
 app.use('/css', expressLess(__dirname + '/assets/less', { compress: true }));
 
 // Index Page
-app.get('/', function (req, res){
-	res.render('index', 
-	  {
-		title: 'Home',
-		partials : {
-			head : '/partials/head'
-		}
-	  });
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Home',
+        partials: {
+            head: '/partials/head'
+        }
+    });
 });
 
-	// All Errors Except for 404 Page.
-	app.use(function(err, req, res, next){
-		var requestedURL = req.protocol + '://' + req.get('Host') + req.url;	
-		console.error(err.stack);
-		console.log(err.stack + '  URL: ' + requestedURL);
-		res.render('error', {title: err.stack});
-	});
+// All Errors Except for 404 Page.
+app.use(function(err, req, res, next) {
+    var requestedURL = req.protocol + '://' + req.get('Host') + req.url;
+    console.error(err.stack);
+    console.log(err.stack + '  URL: ' + requestedURL);
+    res.render('error', { title: err.stack });
+});
 
-	// 404 Error Page. MUST BE LAST (except for server).
-	app.use(function(req, res, next){
-		var requestedURL = req.protocol + '://' + req.get('Host') + req.url;
-		console.log('Error: 404 - ' + requestedURL );
-		res.render('error', {title: '404'});
-	});
+// 404 Error Page. MUST BE LAST (except for server).
+app.use(function(req, res, next) {
+    var requestedURL = req.protocol + '://' + req.get('Host') + req.url;
+    console.log('Error: 404 - ' + requestedURL);
+    res.render('error', { title: '404' });
+});
 
 // Set the Server Up
-var server = app.listen(80, function() {
-	var host = server.address().address
-	var port = server.address().port
-	console.log('App is listening at http://%s:%s', host, port)
+var server = app.listen(8881, function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log('App is listening at http://%s:%s', host, port)
 });
